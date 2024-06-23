40df7605bf314dc9484f854a16509ee4f4d2120b
tappware <tappware@tappwares-MacBook-Pro.local>
2024-06-23 14:37:28 +0600

    Your commit message

--- invoice_app/models.py
+++ invoice_app/models.py
@@ -1,6 +1,17 @@
-from django.db import models
+# from django.db import models
 
 # Create your models here.
 from django import forms
 class UploadFileForm(forms.Form):
     file = forms.FileField()
+
+
+
+
+
+
+# from django.db import models
+#
+# class UploadedFile(models.Model):
+#     file = models.FileField(upload_to='uploads/')
+#     uploaded_at = models.DateTimeField(auto_now_add=True)
--- invoice_app/views.py
+++ invoice_app/views.py
@@ -1,4 +1,5 @@
 # views.py
+from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import UploadFileForm
 from .utils import extract_text_from_file , information_retrieve
@@ -6,60 +7,14 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 import base64
 import pandas as pd
 
-# def upload_file(request):
-#     df_html = None  # Initialize df_html at the start
-#     if request.method == 'POST':
-#         form = UploadFileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             uploaded_files = request.FILES.getlist('files')
-#             uploaded_file: InMemoryUploadedFile = form.cleaned_data['file']
-#
-#             # Read the file in memory
-#             file_data = uploaded_file.read()
-#             file_type = uploaded_file.content_type
-#
-#             # Determine the file type (image or PDF)
-#             if file_type.startswith('image'):
-#                 file_type = 'image'
-#             elif file_type == 'application/pdf':
-#                 file_type = 'pdf'
-#             else:
-#                 return render(request, 'index.html', {
-#                     'form': form,
-#                     'error': 'Unsupported file type.'
-#                 })
-#
-#             # Extract text from the file
-#             extracted_text = extract_text_from_file(file_data, file_type)
-#
-#             # Encode the file data to base64 for display
-#             encoded_file = base64.b64encode(file_data).decode('utf-8')
-#             #----------New Feature: Information Retrieve Button(Button_New)-------
-#             # Generate the DataFrame from the extracted text
-#             df = information_retrieve(extracted_text)
-#             df_html = df.to_html(index=False)
-#
-#             #-----------------
-#
-#             # Return the uploaded file and extracted text in the response
-#             return render(request, 'index.html', {
-#                 'form': form,
-#                 'uploaded_file_data': encoded_file,
-#                 'uploaded_file_name': uploaded_file.name,
-#                 'extracted_text': extracted_text,
-#                 'file_type': file_type,
-#                 'df_html': df_html # Button_New
-#             })
-#     else:
-#         form = UploadFileForm()
-#     return render(request, 'index.html', {'form': form ,'df_html': df_html})
+
 
 
 def upload_file(request):
     # print("Output: " , request.FILES.getlist('files'))
     data = [] # Dictionary to store data for the template
     if request.method == 'POST':
-        form = UploadFileForm((request.POST, request.FILES) or None)
+        form = UploadFileForm(request.POST, request.FILES)
         uploaded_files = request.FILES.getlist('files')
 
         # uploaded_files: InMemoryUploadedFile = form.cleaned_data['files']
@@ -106,10 +61,15 @@ def upload_file(request):
             # })
         print((len(data)))
 
-        # Render the template with data for all uploaded files
-        # return render(request, 'index.html', {'form': form, data})
-        return render(request, 'index.html', {'form': form, 'data': data})
+
+        return render(request, 'index.html', {'form': form, 'data': data })
+
 
     else:
         form = UploadFileForm()
-        return render(request, 'index.html', {'form': form})
+        return render(request, 'index.html', {'form': form, })
+
+
+
+
+
